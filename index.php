41a15997865298f822ddca410a1bf27d45b6be56
blwatkins <blwatkins@users.noreply.github.com>
2025-04-24 12:23:38 -0500

    Add changes from April 24 lecture

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <?php
-function buildColorCard($backgroundColor) {
+function buildColorCard($backgroundColor, $colorName) {
 echo '<div class="card m-2" style="width: 15rem;">';
 echo '<div class="card-body">';
 echo '<div style="background:' . $backgroundColor . '; height: 15rem"></div>';
+echo '<p>' . $colorName . '</p>';
 echo '<button type="button" class="btn btn-danger mt-2">Delete</button>';
 echo '</div>';
 echo '</div>';
@@ -17,8 +18,42 @@ echo '</div>';
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     </head>
     <body>
+        <script>
+            function showForm() {
+                let colorForm = document.getElementById('color-form-container');
+                colorForm.style.display = 'flex';
+            }
+
+            function updateFormColor() {
+                let colorInput = document.getElementById('color-hex');
+                let inputValue = colorInput.value;
+                let hexPattern = /^#[0-9A-Z]{6}$/;
+                let match = hexPattern.test(inputValue);
+                let colorBlock = document.getElementById("form-color-block");
+
+                if (match) {
+                    colorBlock.style.background = inputValue;
+                } else {
+                    colorBlock.style.background = 'black';
+                }
+            }
+        </script>
         <h1 class="text-center m-3">Colors</h1>
         <hr/>
+        <div class="container text-center">
+            <button onclick="showForm();" type="button" class="btn btn-primary mt-2">Add a Color</button>
+        </div>
+        <div id="color-form-container" class="container" style="display: none">
+                <div class="card m-2" style="width: 15rem;">
+                    <div class="card-body">
+                        <div id="form-color-block" style="background: #000000; height: 15rem"></div>
+                        <form id="new-color-form">
+                            <label for="color-hex">Color: </label>
+                            <input type="text" name="color-hex" id="color-hex" oninput="updateFormColor();"/>
+                        </form>
+                    </div>
+                </div>
+        </div>
         <div class="container">
             <?php
                 $jsonString = file_get_contents('./data/colors.json');
@@ -28,29 +63,15 @@ echo '</div>';
                     $colors = json_decode($jsonString, true);
                 }
 
-
                 foreach ($colors as $name => $hex) {
-                    buildColorCard($hex);
+                    $hexColor = htmlentities($hex);
+                    $match = preg_match("/^#[0-9A-Z]{6}$/", $hexColor);
+
+                    if ($match === 1) {
+                        buildColorCard(htmlentities($hex), htmlentities($name));
+                    }
                 }
             ?>
-            <div class="card m-2" style="width: 15rem;">
-                <div class="card-body">
-                    <div style="background: red; height: 15rem"></div>
-                    <button type="button" class="btn btn-danger mt-2">Delete</button>
-                </div>
-            </div>
-            <div class="card m-2" style="width: 15rem;">
-                <div style="background: blue; height: 15rem"></div>
-                <button type="button" class="btn btn-danger">Delete</button>
-            </div>
-            <div class="card m-2" style="width: 15rem;">
-                <div style="background: yellow; height: 15rem"></div>
-                <button type="button" class="btn btn-danger">Delete</button>
-            </div>
-            <div class="card m-2" style="width: 15rem;">
-                <div style="background: green; height: 15rem"></div>
-                <button type="button" class="btn btn-danger">Delete</button>
-            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
     </body>
